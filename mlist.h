c3c870aa6776d28fe6b4eae027718742be233130
marc <wrrzag666@gmail.com>
2013-05-22 21:13:26 +0200

    Added a method to get the first element of the list. 	modified:   mlist.h

--- mlist.h
+++ mlist.h
@@ -3,5 +3,6 @@ typedef struct _mlist MList;
 MList *create_list();
 void delete_list(MList *);
 void add_element(MList *, int);
+int get_first_element(MList *);
 int retrieve_element(MList *, int);
 int get_size(MList *);
